b5dd3face21ed0dd17c1460e90fd2ec92960cf03
aj10972 <jmellreis@gmail.com>
2024-11-05 20:24:37 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,24 +1,29 @@
-
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o maior oceano do mundo?',
+    'O maior oceano do mundo é o Oceano Pacífico'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Charada',
+    'O que é leve como uma pluma, mas nem o homem mais forte consegue segurar?',
+    'O fôlego'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Ciências',
+    'Qual é o elemento químico com o símbolo H?',
+    'O elemento com o símbolo H é o hidrogênio'
+)
+
+criaCartao(
+    'Charada',
+    'O que é que tem muitas chaves, mas não pode abrir portas?',
+    'Um piano'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci'
 )
